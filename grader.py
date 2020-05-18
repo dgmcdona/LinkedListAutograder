e246c08c5acd06d5be9abe72a6b5f22bad840ff6
David McDonald <davidmcdonald@Davids-MacBook-Pro.local>
2020-05-18 04:41:27 -0500

    Prototype complete: pre-autolab-test implementation. Fixed scoreboard and grader.py

--- grader.py
+++ grader.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import sys
+import os
 import json
 from subprocess import check_output, PIPE, CalledProcessError
 
@@ -9,12 +10,15 @@ from subprocess import check_output, PIPE, CalledProcessError
 # Summary: Build and test all Java files.
 # Usage: python3 grader.py
 
+# set the java CLASSPATH to include junit and hamcrest .jar files
 CPFLAGS = '-cp .:libs/hamcrest-all-1.3.jar:libs/junit-4.13.jar'
 
-CLASSES = ['SinglyLinkedList', 'MyStack', 'InfixPostfix']
+# List of test class names w/o file extensions.
 TESTS = ['TestSinglyLinkedList',
          'TestMyStack',
          'TestInfixPostfix']
+
+# Nested dictionary assigning point values to tests.
 POINTS = {'SinglyLinkedList': {
               'testAdd': 4,
               'testInsertAt': 3,
@@ -41,24 +45,27 @@ POINTS = {'SinglyLinkedList': {
           }
 
 
+# Compile test class, return True if successful
 def compile_test(test_class):
     process = 'javac {} {}.java'
     process = process.format(CPFLAGS, test_class)
     try:
         check_output(process, shell=True)
-        return 1
+        return True
     except CalledProcessError:
         print("Could not compile {}".format(test_class))
-        return 0
+        return False
 
 
+# If all test classes do not compile sucessfully, clean and exit.
 def compile_all_tests():
     for test in TESTS:
         if not compile_test(test):
             print("Error compiling test classes: Exiting")
-            sys.exit(2)
+            clean_and_exit(2)
 
 
+# Compile java class file submitted by student, return True if successful.
 def compile_class(_class, scores):
     process = 'javac {} {}.java'
     process = process.format(CPFLAGS, _class)
@@ -70,6 +77,9 @@ def compile_class(_class, scores):
         return False
 
 
+# Test all student-submitted classes. If class compilation fails, assign zeros.
+# Else, update scores dictionary with scores returned by run_test function.
+# Return scores when done.
 def test_all_classes():
     scores = {}
     for _class in POINTS:
@@ -81,30 +91,51 @@ def test_all_classes():
     return scores
 
 
+# If a junit test returns with code 0, all tests were passed, and full points
+# are returned from the values in the scores dictionary. 
+# If exit code is 1, parse failed tests from the output and return scores.
+# Else, something else went wrong with this test, return all zero scores for 
+# the unit tests for the current class.
 def run_test(_class):
     scores = {}
     try:
         process = 'java {} org.junit.runner.JUnitCore {}'
         process = process.format(CPFLAGS, 'Test' + _class)
-        out = check_output(process, shell=True)
-        point_values = POINTS[_class]
-        for test_case in point_values:
-            if test_case in out.decode('utf-8'):
-                scores[test_case] = 0
-            else:
-                scores[test_case] = point_values[test_case]
+        check_output(process, shell=True)
+        scores = POINTS[_class]
         return scores
-    except CalledProcessError:
-        return {key: 0 for (key, value) in point_values.items()}
+    except CalledProcessError as err:
+        if err.returncode == 1:
+            point_values = POINTS[_class]
+            for test_case in point_values:
+                if test_case in err.output.decode('utf-8'):
+                    scores[test_case] = 0
+                else:
+                    scores[test_case] = point_values[test_case]
+            return scores
+        else:
+            return {key: 0 for (key, value) in point_values.items()}
 
 
+# Print JSON representation of the scores.
 def print_scores():
-    print(json.dumps(test_all_classes()))
+    results = test_all_classes()
+    scores = {'scores': results}
+    scores['scoreboard'] = list(results.values())
+    print(json.dumps(scores))
+
+
+# Remove all class and build files, and exit.
+def clean_and_exit(status):
+    os.system('rm *.java')
+    os.system('rm *.class')
+    sys.exit(status)
 
 
 def main():
     compile_all_tests()
     print_scores()
+    clean_and_exit(0)
 
 
 if __name__ == '__main__':
